4f9f39b2543b44b5f543cbdd7c4d270799c8c83d
Cayo Maia <chenrycksm@gmail.com>
2025-07-01 17:36:17 -0300

    feat: Implementa a funcionalidade de toggle para exibir/ocultar contato

--- js/script.js
+++ js/script.js
@@ -4,7 +4,14 @@ const btnReveal = document.getElementById('btn-reveal');
 const contactInfo = document.getElementById('contact-info');
 
 btnReveal.addEventListener('click', () => {
-    contactInfo.style.display = 'block';
+    contactInfo.classList.toggle('visivel');
 
-    btnReveal.textContent = 'Ocultar contato';
+    const isVisible = contactInfo.classList.contains('visivel');
+
+    if (isVisible) {
+        btnReveal.textContent = 'Ocultar Contato';
+    } else {
+        btnReveal.textContent = 'Mostrar Contato'
+    }
+
 })
